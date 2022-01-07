7531bd5bdf05ad0cd8bb548b54523526f75f700c
dongduu <4012popo@naver.com>
2022-01-07 23:44:44 +0900

    frat: practice react router

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,16 @@
-export default function About() {
-  return <div>About 페이지입니다.</div>;
+import queryString from "query-string";
+
+export default function About(props) {
+  const seachParams = props.location.search;
+  console.log(seachParams);
+  // const obj = new URLSearchParams(seachParams);
+  // console.log(obj.get("name"));
+  const query = queryString.parse(seachParams);
+  console.log(query);
+  return (
+    <div>
+      <h2>About 페이지입니다.</h2>
+      {query.name && <p>name은 {query.name}입니다.</p>}
+    </div>
+  );
 }
